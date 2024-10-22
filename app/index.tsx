5273de1c92231b3ffd28b955dfd6771125442976
AleksBartov <37273772+AleksBartov@users.noreply.github.com>
2024-10-22 09:44:09 +0300

    adding icons

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,31 @@
 import { View, useWindowDimensions, ScrollView } from "react-native";
 import React from "react";
-import { Canvas, LinearGradient, Rect, vec } from "@shopify/react-native-skia";
+import {
+  Canvas,
+  Image,
+  LinearGradient,
+  Rect,
+  useImage,
+  vec,
+} from "@shopify/react-native-skia";
 import Day from "../components/Day";
 import { dayHeight, Days, dayWidth } from "../constants/Days";
 import Animated, {
+  FadeInDown,
+  FadeOut,
+  Layout,
   useAnimatedRef,
+  useDerivedValue,
   useScrollViewOffset,
+  useSharedValue,
 } from "react-native-reanimated";
 
 const index = () => {
   const animatedRef = useAnimatedRef();
   const scrollOffset = useScrollViewOffset(animatedRef);
   const { width, height } = useWindowDimensions();
+
+  const prepSergyImg = useImage(require("../assets/images/prepSergy.jpg"));
   return (
     <>
       <Canvas style={{ flex: 1 }}>
@@ -52,6 +66,27 @@ const index = () => {
           })}
         </Animated.ScrollView>
       </View>
+      <Animated.View
+        style={{
+          position: "absolute",
+          bottom: 0,
+          width: "100%",
+          height: height * 0.65,
+        }}
+        entering={FadeInDown.delay(500).duration(600)}
+        exiting={FadeOut}
+      >
+        <Canvas style={{ flex: 1 }}>
+          <Image
+            image={prepSergyImg}
+            fit="contain"
+            x={0}
+            y={0}
+            width={130}
+            height={130}
+          />
+        </Canvas>
+      </Animated.View>
     </>
   );
 };
--- constants/Days.ts
+++ constants/Days.ts
@@ -8,6 +8,65 @@ export const Days = [
   {
     name: "пн",
     date: "21",
+    image: require("../assets/images/prepSergy.jpg"),
+    title: "преподобный Сергий Радонежский",
+    text: "Lorem ipsum dolor sit amet. Eos excepturi error 33 deleniti reiciendis et odit nisi qui voluptatem excepturi qui amet mollitia. Non assumenda eligendi qui ullam accusantium et soluta nihil. Ut voluptas perferendis hic corrupti pariatur et perspiciatis doloremque est commodi ullam. Ut natus autem est alias provident non omnis quia est praesentium delectus aut voluptas labore et suscipit aliquid est debitis saepe? Ex possimus quia aut quod tenetur qui omnis soluta! Non architecto debitis non reiciendis numquam eos internos consequuntur aut accusamus nemo ut quia quisquam At molestias asperiores At tempore sint. Eos nemo nihil et quae iusto est sapiente iure ea aspernatur praesentium ea earum laborum. Id officiis distinctio ut temporibus minus qui aperiam consequatur. Et perferendis necessitatibus et illo ducimus est expedita rerum? Qui voluptatem quia est voluptas reprehenderit sed natus magnam aut exercitationem culpa.",
+  },
+  {
+    name: "вт",
+    date: "22",
+  },
+  {
+    name: "ср",
+    date: "23",
+  },
+  {
+    name: "чт",
+    date: "24",
+  },
+  {
+    name: "пт",
+    date: "25",
+  },
+  {
+    name: "сб",
+    date: "26",
+  },
+  {
+    name: "вс",
+    date: "27",
+  },
+  {
+    name: "пн",
+    date: "21",
+  },
+  {
+    name: "вт",
+    date: "22",
+  },
+  {
+    name: "ср",
+    date: "23",
+  },
+  {
+    name: "чт",
+    date: "24",
+  },
+  {
+    name: "пт",
+    date: "25",
+  },
+  {
+    name: "сб",
+    date: "26",
+  },
+  {
+    name: "вс",
+    date: "27",
+  },
+  {
+    name: "пн",
+    date: "21",
   },
   {
     name: "вт",
